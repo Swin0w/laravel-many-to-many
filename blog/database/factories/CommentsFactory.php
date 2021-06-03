7108fb1a1e3f51d9bff5293bc0124f1d67909422
Simone <marzolla.istruzione@gmail.com>
2021-06-03 03:43:42 +0200

    esercizio concluso

--- blog/database/factories/CommentsFactory.php
+++ blog/database/factories/CommentsFactory.php
@@ -7,6 +7,6 @@ use Faker\Generator as Faker;
 
 $factory->define(Comments::class, function (Faker $faker) {
     return [
-        'text' => $faker -> sentece
+        'text' => $faker -> sentence
     ];
 });
--- blog/database/seeds/CommentsSeeder.php
+++ blog/database/seeds/CommentsSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 
+use App\Post;
+use App\Comments;
+
 class CommentsSeeder extends Seeder
 {
     /**
@@ -11,6 +14,11 @@ class CommentsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(Comments::class, 500) -> make()
+            -> each(function($comment){
+                $post = Post::inRandomOrder() -> first();
+                $comment -> post() -> associate($post);
+                $comment -> save();
+            });
     }
 }
